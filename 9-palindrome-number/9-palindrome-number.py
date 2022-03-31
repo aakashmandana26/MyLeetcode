efda85ff98022fcbc9221522174f41c06e80875f
aakashmandana26 <73030921+aakashmandana26@users.noreply.github.com>
2022-03-31 21:56:06 +0530

    Time: 199 ms (5.03%), Space: 13.9 MB (18.57%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        n=x
+        m=x
+        count=0
+        rem=0
+        if(x<0):
+            return False
+        else:
+            while(n//10 != 0):
+                n //= 10
+                count += 1
+            while(count>=0):
+                rem += m%10 * (10**count)
+                m //= 10
+                count -= 1
+            if(rem == x):
+                return True
+            else: 
+                return False
+            
